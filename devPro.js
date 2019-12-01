1f6bc0ffa472db1f7c8d475f20932263f388989e
Irtaza Malik <irtz.malik@gmail.com>
2019-12-01 03:13:15 -0500

    fixed issues in dev_pro

--- devPro.js
+++ devPro.js
@@ -107,27 +107,5 @@ pdf.create(result, options).toFile('abc.pdf', function(err, res) {// making pdf
 };
 
 
-// Find a devPro show
-/*
-devPro.prototype.findShow = function(show) {
-  const URL = "http://api.devPromaze.com/singlesearch/shows?q=" + show;
-
-  axios.get(URL).then(function(response) {
-    const showData = [
-      "Show: " + response.data.person.name,
-      "Genre(s): " + response.data.genres.join(", "),
-      "Rating: " + response.data.rating.average,
-      "Network: " + response.data.network.name,
-      "Summary: " + response.data.summary,
-      "-".repeat(60)
-    ].join("\n\n");
-
-    fs.appendFile("log.txt", showData, err => {
-      if (err) throw err;
-
-      console.log(showData);
-    });
-  });
-};*/
 
 module.exports = devPro;
